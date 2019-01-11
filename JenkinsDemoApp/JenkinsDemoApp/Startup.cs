9c2dc45476e546e15b3c28148666ad36443c1e2e
rohantangade <rohantangade@gmail.com>
2019-01-12 00:15:59 +0530

    First commit

---
+++ JenkinsDemoApp/JenkinsDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsDemoApp.Startup))]
+namespace JenkinsDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
